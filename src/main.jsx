401cea191a00f1ef4e2e0fa593d2663cf6fa4a9a
claritycost <hello@claritycosts.co.uk>
2026-02-23 14:04:19 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Landing from './pages/Landing'
-import Start from './pages/Start'
-import Calculating from './pages/Calculating'
-import Results from './pages/Results'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import Landing from "./pages/Landing";
+import Start from "./pages/Start";
+import Calculating from "./pages/Calculating";
+import Results from "./pages/Results";
+import Save from "./pages/Save";
+
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/start" element={<Start />} />
+        <Route path="/"            element={<Landing />} />
+        <Route path="/start"       element={<Start />} />
         <Route path="/calculating" element={<Calculating />} />
-        <Route path="/results" element={<Results />} />
+        <Route path="/results"     element={<Results />} />
+        <Route path="/save"        element={<Save />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
-)
+  </StrictMode>
+);
