8d6cda1cbb6e6440753d4d665885749d83a930e6
claritycost <hello@claritycosts.co.uk>
2026-02-28 21:07:07 +0000

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -10,10 +10,16 @@ import Upgrade from './pages/Upgrade.jsx'
 import Success from './pages/Success.jsx'
 import Admin from './pages/Admin.jsx'
 import Share from './pages/Share.jsx'
+import PrivacyPolicy from './pages/PrivacyPolicy.jsx'
+import CookiePolicy from './pages/CookiePolicy.jsx'
+import Terms from './pages/Terms.jsx'
+import About from './pages/About.jsx'
+import CookieBanner from './components/CookieBanner.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+      <CookieBanner />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/start" element={<Start />} />
@@ -24,6 +30,10 @@ createRoot(document.getElementById('root')).render(
         <Route path="/success" element={<Success />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/share/:id" element={<Share />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/cookie-policy" element={<CookiePolicy />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
